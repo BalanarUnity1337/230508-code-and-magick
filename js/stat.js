54c01acb87eb70239d4462a55f3f7ca372a1ccf1
Rush3D <vanesdemon96@gmail.com>
2017-01-23 20:42:35 +0700

    Произведен рефакторинг кода

--- js/stat.js
+++ js/stat.js
@@ -1,7 +1,30 @@
 'use strict';
-
+/**
+ *
+ * @param context ctx Контекст рисования на холсте
+ * @param array names Массив имен победителей
+ * @param array times Массив времени прохождения
+ */
 window.renderStatistics = function (ctx, names, times) {
-  var drawCloud = function (x, y, width, height) {
+  var HISTOGRAM_HEIGHT = 150;
+  var HISTOGRAM_X = 140;
+  var COLUMN_WIDTH = 40;
+  var COLUMN_INDENT = 50;
+  var step = HISTOGRAM_HEIGHT / getMaxTime();
+  drawCloud(110, 20, 420, 270, 'rgba(0, 0, 0, 0.7');
+  drawCloud(100, 10, 420, 270, '#ffffff');
+  renderText();
+  renderHistogram();
+
+  /**
+   *
+   * @param int x Начальная координата облака по оси X
+   * @param int y Начальная координата облака по оси Y
+   * @param int width Ширина облака
+   * @param int height Ширина облака
+   * @param string fillColor Цвет заливки облака
+   */
+  function drawCloud (x, y, width, height, fillColor) {
     var offset = 15;
     ctx.beginPath();
     ctx.moveTo(x, y);
@@ -14,50 +37,79 @@ window.renderStatistics = function (ctx, names, times) {
     ctx.lineTo(x + width / 2, y + offset);
     ctx.closePath();
     ctx.stroke();
+    ctx.fillStyle = fillColor;
     ctx.fill();
-  };
-
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  drawCloud(110, 20, 420, 270);
-
-  ctx.fillStyle = '#ffffff';
-  drawCloud(100, 10, 420, 270);
-
-  ctx.font = '16px PT Mono';
-  ctx.fillStyle = '#000000';
-  ctx.fillText('Ура вы победили!', 220, 40);
-  ctx.fillText('Список результатов:', 220, 60);
-
-  var max = -1;
-
-  for (var i = 0; i < times.length; i++) {
-    var time = times[i];
-    if (time > max) {
-      max = time;
-    }
   }
 
-  var histogramHeight = 150;
-  var histogramX = 140;
-  var columnWidth = 40;
-  var columnIndent = 50;
-  var step = histogramHeight / max;
+  /**
+   * Отрисовка текста вверху облака
+   * в случае победы
+   */
+  function renderText() {
+    ctx.font = '16px PT Mono';
+    ctx.fillStyle = '#000000';
+    ctx.fillText('Ура вы победили!', 220, 40);
+    ctx.fillText('Список результатов:', 220, 60);
+  }
 
-  for (i = 0; i < times.length; i++) {
-    var name = names[i];
-    time = times[i];
-    var height = step * time;
+  /**
+   * Поиск максимального значения
+   * времени в массиве times
+   *
+   * @returns {number}
+   */
+  function getMaxTime() {
+    var max = -1;
+    for (var i = 0; i < times.length; i++) {
+      var time = times[i];
+      if (time > max) {
+        max = time;
+      }
+    }
+    return max;
+  }
 
-    ctx.fillText(time.toFixed(0), histogramX + (columnIndent + columnWidth) * i, histogramHeight - height + 85);
-
-    if (name === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    } else {
-      ctx.fillStyle = 'rgba(0, 0, ' + ((Math.random() * 5 * 50)).toFixed(0) + ', ' + (Math.random() + 0.1).toFixed(1) + ')';
+  /**
+   * Отрисовка гистограммы
+   */
+  function renderHistogram() {
+    for (var i = 0; i < times.length; i++) {
+      var name = names[i];
+      var time = times[i];
+      var height = step * time;
+      renderHistogramTextTop();
+      renderHistogramTextBottom();
+      if (name === 'Вы') {
+        ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+      } else {
+        ctx.fillStyle = 'rgba(0, 0, ' + ((Math.random() * 5 * 50)).toFixed(0) + ', ' + (Math.random() + 0.1).toFixed(1) + ')';
+      }
+      renderHistogramColumn();
     }
 
-    ctx.fillRect(histogramX + (columnIndent + columnWidth) * i, 245, columnWidth, height * -1);
-    ctx.fillStyle = '#000000';
-    ctx.fillText(name, histogramX + (columnIndent + columnWidth) * i, histogramHeight + 110);
+    /**
+     * Отрисовка текущего столбца гистограммы
+     */
+    function renderHistogramColumn() {
+      ctx.fillRect(HISTOGRAM_X + (COLUMN_INDENT + COLUMN_WIDTH) * i, 245, COLUMN_WIDTH, height * -1);
+    }
+
+    /**
+     * Размещение времени прохождения
+     * над текущим столбцом гистограммы
+     */
+    function renderHistogramTextTop() {
+      ctx.fillStyle = '#000000';
+      ctx.fillText(time.toFixed(0), HISTOGRAM_X + (COLUMN_INDENT + COLUMN_WIDTH) * i, HISTOGRAM_HEIGHT - height + 85);
+    }
+
+    /**
+     * Размещение имени игрока
+     * под текущим столбцом гистограммы
+     */
+    function renderHistogramTextBottom() {
+      ctx.fillStyle = '#000000';
+      ctx.fillText(name, HISTOGRAM_X + (COLUMN_INDENT + COLUMN_WIDTH) * i, HISTOGRAM_HEIGHT + 110);
+    }
   }
 };
